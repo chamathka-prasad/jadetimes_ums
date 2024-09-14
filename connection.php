1619e4d6731580c1861b20a65c0a864483a6545a
k.d. chamathka prasad kumara <chamathkaprasad123@gmail.com>
2024-09-14 21:57:16 +0530

    2nd-commit

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@ class Database
 {
     public static function operation($q, $type)
     {
-        $connection = new mysqli("localhost", "jadetime_crm_root", "ri9g7xDYGCcv","jadetime_ums", "3306");
+      
        
 
         if ($type == "iud") {
